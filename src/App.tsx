a4b493eae17bcae4dc9de4693d04d8e0d2196b29
Keaton-Brewster <keaton.brewster@gmail.com>
2022-09-09 12:03:59 -0500

    added React-bootstrap elements to create a more stylized version of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React, { ReactElement } from "react";
 import QRCode from "./Components/QRCode";
 
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 import "./App.css";
 
 export default function App(): ReactElement {
+  const { Brand, Toggle, Collapse } = Navbar;
   return (
-    <>
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Brand>QRCode Generator</Brand>
+        </Container>
+      </Navbar>
       <QRCode />
-    </>
+    </Container>
   );
 }
