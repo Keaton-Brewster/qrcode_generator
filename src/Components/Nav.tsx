e7d7b8d869f0a483ee13977454b1e0c0c597ad7e
Keaton-Brewster <keaton.brewster@gmail.com>
2022-09-10 15:38:50 -0500

    striking through 'saved codes' dropdown for now until it is useable

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,18 +1,22 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">QRCodes</Navbar.Brand>
+        <Navbar.Brand href="/">QRCodes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">New Codes</Nav.Link>
-            <Nav.Link href="#link">Saved Codes</Nav.Link>
+            <Nav.Link disabled href="/saved">
+              <span style={{ textDecoration: "line-through" }}>
+                Saved Codes
+              </span>{" "}
+              - Coming Soon!
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
