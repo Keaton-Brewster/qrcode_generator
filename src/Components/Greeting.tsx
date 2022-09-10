8968119b637bbbfebc2bf4a0638b76f15a7451ce
Keaton-Brewster <keaton.brewster@gmail.com>
2022-09-10 15:17:30 -0500

    added a greeting comp, that displays a message on the main screen

---
+++ src/Components/Greeting.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CSS/greeting.css";
+
+export default function Greeting() {
+  return (
+    <>
+      <h1 className="greeting">Welcome!</h1>
+      {"\n"}
+      <p className="greeting">Create your own custom QR Code for free!</p>
+    </>
+  );
+}
