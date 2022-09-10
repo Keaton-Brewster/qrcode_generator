57bb2735739606007bd916c42f5ef1714537bc92
Keaton-Brewster <keaton.brewster@gmail.com>
2022-09-10 15:17:23 -0500

    ådded a greeting comp, that displays a message on the main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import QRCode from "./Components/QRCode";
 import Nav from "./Components/Nav";
+import Greeting from "./Components/Greeting";
 // import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-
-
 
 export default function App() {
   return (
     <>
       <Nav />
+      <Greeting />
       <QRCode />
     </>
   );
